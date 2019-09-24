9f9f57c1b7ef4ce11c5c212a63ca28a62b05525b
Gurpreet Singh <thegurpreetsingh44@gmail.com>
2019-09-24 15:41:45 +0530

    dgh

--- realestate/settings.py
+++ realestate/settings.py
@@ -167,6 +167,10 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
 )
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
 
 
 # Media Folder Settings
